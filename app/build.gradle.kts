fc666fe229e644555fe2e5d026b3aa007761e1b1
Victor B. <contact@itsvic.dev>
2025-03-13 09:30:04 +0100

    1.0.0 Beta 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,9 @@ plugins {
 val versionMajor = 1
 val versionMinor = 0
 val versionPatch = 0
-val versionBuild = 3 // Hidden from the user
+val versionBuild = 4 // Hidden from the user
 
-val versionSuffix = "Beta 1"
+val versionSuffix = "Beta 2"
 
 val appVersionCode = ((versionMajor * 100 + versionMinor) * 100 + versionPatch) * 1000 + versionBuild
 val appVersionName =
