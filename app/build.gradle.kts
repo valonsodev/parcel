e1cfc1e5573a2f1761737a636e79c7fb308705d2
Victor B. <contact@itsvic.dev>
2025-03-15 07:35:04 +0100

    1.0.0 RC 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,9 +15,9 @@ plugins {
 val versionMajor = 1
 val versionMinor = 0
 val versionPatch = 0
-val versionBuild = 7 // Hidden from the user
+val versionBuild = 8 // Hidden from the user
 
-val versionSuffix = "RC 2"
+val versionSuffix = "RC 2.1"
 
 val appVersionCode =
     ((versionMajor * 100 + versionMinor) * 100 + versionPatch) * 1000 + versionBuild
