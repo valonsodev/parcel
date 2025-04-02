79df355d3a737389e35c5268075ebc3a12606389
Victor B. <contact@itsvic.dev>
2025-04-02 20:43:09 +0200

    NotificationWorker: remove NetworkFailure status check
    this check is pointless because this function never returns a parcel with NetworkError. such an error status is created only by the UI layer and isn't a real status a parcel can have.
    we already handle errors by skipping them, so this is harmless

--- app/src/main/java/dev/itsvic/parceltracker/NotificationWorker.kt
+++ app/src/main/java/dev/itsvic/parceltracker/NotificationWorker.kt
@@ -11,7 +11,6 @@ import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
-import dev.itsvic.parceltracker.api.Status
 import dev.itsvic.parceltracker.api.getParcel
 import dev.itsvic.parceltracker.db.ParcelStatus
 import kotlinx.coroutines.Dispatchers
@@ -45,14 +44,6 @@ class NotificationWorker(context: Context, params: WorkerParameters) :
                     getParcel(parcel.parcelId, parcel.postalCode, parcel.service)
                 } catch (e: Exception) {
                     Log.d("NotificationWorker", "Failed to fetch, skipping", e)
-                    continue
-                }
-
-                if (apiParcel.currentStatus == Status.NetworkFailure) {
-                    Log.d(
-                        "NotificationWorker",
-                        "Parcel status is NetworkFailure, skipping."
-                    )
                     continue
                 }
 
