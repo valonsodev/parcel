bbe8e7dbb9ce011b336a9ae98d4e81102d7d54d9
Victor B. <contact@itsvic.dev>
2025-04-05 16:36:54 +0200

    chore/misc: add Czech to DefaultRegions.kt with a comment explaining why it's there

--- app/src/main/java/dev/itsvic/parceltracker/misc/DefaultRegions.kt
+++ app/src/main/java/dev/itsvic/parceltracker/misc/DefaultRegions.kt
@@ -1,8 +1,13 @@
 package dev.itsvic.parceltracker.misc
 
+// This is required for the UPS backend, because for some reason it requires
+// the region in the locale, and Java does not provide that info unless it was
+// provided to it beforehand.
+// tldr i18n sucks
 val defaultRegionsForLanguageCode = mapOf(
     "pl" to "PL",
     "hu" to "HU",
     "uk" to "UA",
     "th" to "TH",
+    "cs" to "CZ"
 )
