f34b802dcacc4cb122be46951c9d33e5368c8f46
Wiktor Dudek <wiktor.dudek.it@gmail.com>
2025-06-23 17:40:35 +0200

    Add InPost as a delivery service option

--- app/src/main/java/dev/itsvic/parceltracker/api/Core.kt
+++ app/src/main/java/dev/itsvic/parceltracker/api/Core.kt
@@ -48,6 +48,7 @@ enum class Service {
   UKRPOSHTA,
   POSTNORD,
   ALLEGRO_ONEBOX,
+  INPOST,
 
   // Asia
   EKART,
@@ -89,6 +90,7 @@ fun getDeliveryService(service: Service): DeliveryService? {
     Service.UKRPOSHTA -> UkrposhtaDeliveryService
     Service.POSTNORD -> PostNordDeliveryService
     Service.ALLEGRO_ONEBOX -> AllegroOneBoxDeliveryService
+    Service.INPOST -> InPostDeliveryService
 
     Service.EKART -> EKartDeliveryService
     Service.SPX_TH -> SPXThailandDeliveryService
