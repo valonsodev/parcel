cf8dd57edd101b0445cfa48853315924c1505371
Wiktor Dudek <wiktor.dudek.it@gmail.com>
2025-06-30 15:49:46 +0200

    use shiny new statuses for event code mapping

--- app/src/main/java/dev/itsvic/parceltracker/api/InPostDeliveryService.kt
+++ app/src/main/java/dev/itsvic/parceltracker/api/InPostDeliveryService.kt
@@ -60,31 +60,77 @@ object InPostDeliveryService : DeliveryService {
 
   private fun eventCodeToStatus(eventCode: String): Status {
     // All event codes: https://developers.inpost-group.com/tracking-events
+    // COD: Cash On Delivery
+    // PUDO: Pick-Up Drop-Off
+    // APM: Automated Parcel Machine
+
     return when (eventCode) {
-      "CRE.1001" -> Status.Preadvice
-      "CRE.1002" -> Status.AwaitingPickup
-      "FMD.1001" -> Status.AwaitingPickup
-      "FMD.1002" -> Status.PickedUp
-      "FMD.1003" -> Status.InTransit
-      "FMD.1004" -> Status.PickedUp
-      "FMD.1005" -> Status.PickedUp
-      "MMD.1001" -> Status.InWarehouse
-      "MMD.1003" -> Status.InTransit
-      "MMD.1004" -> Status.InTransit
-      "LMD.1001" -> Status.InTransit
-      "LMD.1002" -> Status.Delivered
-      "LMD.1003" -> Status.AwaitingPickup
-      "LMD.1004" -> Status.AwaitingPickup
-      "LMD.1005" -> Status.AwaitingPickup
-      "RTS.1001" -> Status.DeliveryFailure
-      "RTS.1002" -> Status.Delivered
-      "FUL.1003" -> Status.OutForDelivery
-      "EOL.1001" -> Status.Delivered
-      "EOL.1002" -> Status.PickedUp
-      "EOL.1003" -> Status.Delivered
-      "EOL.1004" -> Status.Delivered
-      "EOL.1005" -> Status.Delivered
-      "HAN.1001" -> Status.InTransit
+      "CRE.1001" -> Status.Preadvice // [Generic] Parcel Creation
+      "CRE.1002" -> Status.AwaitingPickup // 	[Generic] Ready for acceptance
+      "FMD.1001" -> Status.LockerboxAcceptedParcel // [Generic] Ready for courier collection
+      "FMD.1002" -> Status.InTransit // [Generic] Collected by courier
+      "FMD.1003" -> Status.InTransit // [Generic] In-transit (first-mile)
+      "FMD.1004" -> Status.PickedUpByCourier // Collected by courier in PUDO
+      "FMD.1005" -> Status.PickedUpByCourier // Collected by courier in APM
+      "MMD.1001" -> Status.InWarehouse // [Generic] Adopted at Logistics Centre
+      "MMD.1002" -> Status.InWarehouse // [Generic] Processed at Logistics Centre
+      "MMD.1003" -> Status.InTransit // [Generic] Dispatched from Logistics Centre
+      "MMD.1004" -> Status.InTransit // [Generic] Line-Haul
+      "LMD.1001" -> Status.InTransit // [Generic] In-transit (last-mile)
+      "LMD.1002" -> Status.Delivered // [Generic] Arrived at destination
+      "LMD.1003" -> Status.AwaitingPickup // [Generic] Ready to collect
+      "LMD.1004" -> Status.AwaitingPickup // Ready to collect PUDO
+      "LMD.1005" -> Status.AwaitingPickup // Ready to collect APM
+      "LMD.3002" -> Status.Readdressed // Alternative temporary collection point assigned
+      "LMD.3003" -> Status.Readdressed // Alternative collection point assigned
+      "LMD.3004" -> Status.InTransit // Branch collection assigned
+      "LMD.3005" -> Status.Readdressed // Original collection point reassigned
+      "LMD.3006" -> Status.Readdressed // Delivery readdressed
+      "LMD.3007" -> Status.AwaitingPickup // Stored temporary in a service point
+      "LMD.3008" -> Status.DeliveryFailure // Expired stored parcel
+      "LMD.3009" -> Status.DeliveryFailure // Expired on temporary box machine
+      "LMD.3010" -> Status.DeliveryFailure // Expired on temporary box machine
+      "LMD.3011" -> Status.DeliveryFailure // Expired on temporary collection point
+      "LMD.3012" ->
+          Status
+              .DeliveryFailure // Redirect cancelled, The redirection of the parcel wasn’t possible
+      "LMD.3013" -> Status.Readdressed // Redirected to PUDO
+      "LMD.3014" -> Status.Readdressed // Redirected to APM
+      "CC.001" -> Status.Customs // Parcel at customs
+      "CC.002" -> Status.CustomsSuccess // Parcel customs cleared
+      "CC.003" -> Status.CustomsHeld // Parcel held at customs
+      "LMD.9001" -> Status.PickupTimeEndingSoon // Reminder to collect
+      "LMD.9002" -> Status.DeliveryFailure // Expired
+      "LMD.9003" -> Status.DeliveryFailure // Oversized
+      "LMD.9004" -> Status.DeliveryFailure // [Generic] Attempted delivery
+      "LMD.9005" -> Status.DeliveryFailure // [Generic] Undeliverable
+      "LMD.9006" -> Status.DeliveryFailure // Undeliverable: Rejected by recipient
+      "LMD.9007" -> Status.DeliveryFailure // Undeliverable: Incorrect delivery details
+      "LMD.9008" -> Status.DeliveryFailure // Undeliverable: Receiver unknown
+      "LMD.9009" -> Status.DeliveryFailure // Undeliverable: COD conditions not met
+      "LMD.9010" -> Status.DeliveryFailure // Undeliverable: No mailbox
+      "LMD.9011" -> Status.DeliveryFailure // Undeliverable: No access to location
+      "LMD.9012" -> Status.AwaitingPickup // Stored temporary in a box machine
+      "LMD.9013" -> Status.AwaitingPickup // Parcel ready to collect at customer service point
+      "EOL.1001" -> Status.Delivered // [Generic] Delivered
+      "EOL.1002" -> Status.PickedUp // Parcel collected
+      "EOL.1003" -> Status.DeliveredToASafePlace // Delivered at Safe Place
+      "EOL.1004" -> Status.DeliveredToNeighbor // Delivered at neighbour
+      "EOL.1005" -> Status.Delivered // Delivered with verified recipient
+      "EOL.9001" -> Status.DeliveryFailure // Missing
+      "EOL.9002" -> Status.Damaged // Damaged
+      "EOL.9003" -> Status.Destroyed // Destroyed
+      "EOL.9004" -> Status.DeliveryFailure // Cancelled
+      "RTS.1001" -> Status.ReturningToSender // [Generic] Returning to Sender
+      "RTS.1002" -> Status.ReturnedToSender // [Generic] Returned to Sender
+      "FUL.1001" -> Status.InWarehouse // [Generic] Picked
+      "FUL.1002" -> Status.InWarehouse // [Generic] Packed
+      "FUL.1003" -> Status.OutForDelivery // [Generic] Dispatched
+      "INF.1001" -> Status.Unknown // FIXME: COD payment received. Is this even displayed on
+      // FIXME: the tracking site? I guess it may be their internal event code. Anyway, I
+      // FIXME: don't see any matching status codes for this one
+      "INF.9001" -> Status.Delayed // Delay in Delivery
+      "HAN.1001" -> Status.InTransit // [Generic] Handover
       else -> logUnknownStatus("InPost", eventCode)
     }
   }
